22443e4f6cb48efe0733e4185011e0351f9680ac
Aron Jon Prenovost <Aroniascoder@gmail.com>
2023-01-26 08:22:59 -0800

    updates

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## template
+https://www.tooplate.com/view/2132-clean-work
+
 ## hosting / form submission / email notification
 https://www.netlify.com/
 
--- js/custom.js
+++ js/custom.js
@@ -32,6 +32,35 @@
         }, 300);
       }
     });
+
+    // change testimonials automatically
+    // disable after onclick event
+    window.addEventListener("load", (event) => {
+        const radios = document.querySelector('.testimonials-section').querySelectorAll('.slider__nav');
+
+        // stop setInterval if human click
+        for (var i = 0; i < radios.length; i++) {
+          radios[i].addEventListener('click', function(event) {
+            if (event.isTrusted) {
+              clearInterval(testimonialsTimer);
+            }
+          });
+        }
+
+        var count = 0;
+        var testimonialsTimer = setInterval(function() {
+            var radio = radios[count];
+            if (radio) {
+                radio.click();
+            }
+            
+            if (count === radios.length) {
+                count = 0;
+            } else {
+                count++;
+            }
+        }, 5000);
+    });
     
   })(window.jQuery);
 
